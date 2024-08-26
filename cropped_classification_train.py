d9f5adf9169f0a30695338e989996578eb62aa30
Demir Arikan <dmrarikan@gmail.com>
2024-08-26 19:47:39 -0400

    updated path to model

--- cropped_classification_train.py
+++ cropped_classification_train.py
@@ -2,6 +2,6 @@ from ultralytics import YOLO
 
 model = YOLO("yolov8n-cls.pt")
 
-model.train(data="/home/demir/Desktop/vein-puncture-detection-egg/data/cropped_images",  name="cropped_detection")
+model.train(data="/home/demir/Desktop/vein-puncture-detection-egg/data/cropped_images",  name="cropped_detection", epochs=50)
 
 # TODO: change path to dataset folder!!
