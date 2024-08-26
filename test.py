3a4580d4977eb514318b25532c471f09de2ef4fe
Demir Arikan <dmrarikan@gmail.com>
2024-08-26 19:03:24 -0400

    added testing functions for classification and object detection networks

--- test.py
+++ test.py
@@ -1,5 +1,12 @@
 from ultralytics import YOLO
 
+#classification model
 model = YOLO("/home/demir/Desktop/jhu_project/vein_puncture/runs/classify/mojtaba_test/weights/best.pt")
 
 validation_results = model.val(data="/home/demir/Desktop/jhu_project/vein_puncture/puncture_detection_dataset", imgsz=640, split='test')
+
+
+# object detection model
+# model = YOLO("/home/demir/Desktop/vein-puncture-detection-egg/runs/detect/train7/weights/best.pt")
+
+# validation_results = model.val(data="/home/demir/Desktop/vein-puncture-detection-egg/data/puncture_detection.v4i.yolov8/data.yaml", split='test')
